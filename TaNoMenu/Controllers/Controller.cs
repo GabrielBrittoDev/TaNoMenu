4d29e927142151a268afbae0516b51c586ed4286
Gabriel Leal <gabrielleao.mb@gmail.com>
2021-05-31 18:46:52 -0300

    Adicionando validacoes para o controller de estabelecimentos

--- TaNoMenu/Controllers/Controller.cs
+++ TaNoMenu/Controllers/Controller.cs
@@ -19,6 +19,8 @@ namespace TaNoMenu.Controllers
                         return Unauthorized(httpException.Message); 
                     case 404:
                         return NotFound(httpException.Message);
+                    default:
+                        return BadRequest(httpException.Message);
                 }
             }
 
